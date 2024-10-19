9f1a7c903faf748cccea8cd9a2d8fe76fb179771
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-19 19:34:09 +0600

    Initial commit

---
+++ src/test/java/com/shahariyr/RestCrudApi/RestCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahariyr.RestCrudApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
