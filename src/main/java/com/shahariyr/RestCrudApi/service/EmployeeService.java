7580c636f5264339c68c013fa6ea4970c0a1048e
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-22 00:10:42 +0600

    add service layer

---
+++ src/main/java/com/shahariyr/RestCrudApi/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.shahariyr.RestCrudApi.service;
+
+import java.util.List;
+
+import com.shahariyr.RestCrudApi.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findall();
+}
