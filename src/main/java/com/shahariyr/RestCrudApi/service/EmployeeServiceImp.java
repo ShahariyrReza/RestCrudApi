937e0c174cf6edd4ac7a65c913efc93f490df851
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-24 00:10:25 +0600

    update method

--- src/main/java/com/shahariyr/RestCrudApi/service/EmployeeServiceImp.java
+++ src/main/java/com/shahariyr/RestCrudApi/service/EmployeeServiceImp.java
@@ -3,6 +3,7 @@ package com.shahariyr.RestCrudApi.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.shahariyr.RestCrudApi.dao.EmployeeDAO;
 import com.shahariyr.RestCrudApi.entity.Employee;
@@ -13,7 +14,7 @@ public class EmployeeServiceImp implements EmployeeService{
 	
 	
 	//Constructor Injection of employeeDao
-	private EmployeeDAO employeeDAO;
+	private EmployeeDAO employeeDAO; // ekhn e ami EmployeeDao k "employeeDao" hisebe niyesi.. so niche return korte hobe ei akare
 	
 	public EmployeeServiceImp(EmployeeDAO thEmployeeDAO) {
 		this.employeeDAO = thEmployeeDAO;
@@ -23,6 +24,29 @@ public class EmployeeServiceImp implements EmployeeService{
 	@Override
 	public List<Employee> findall() {
 		return employeeDAO.findall();
+	}
+
+
+	@Override
+	public Employee findById(int theId) {
+		
+		return employeeDAO.findById(theId);
+	}
+
+
+	@Transactional
+	@Override
+	public Employee savEmployee(Employee thEmployee) {
+		
+		return employeeDAO.savEmployee(thEmployee);
+	}
+
+
+	@Transactional
+	@Override
+	public void deleteEmployee(int theId) {
+		employeeDAO.deleteEmployee(theId);
+		
 	}
 
 	
