cb9ec10cf3ed39b579fe8c64401e6b38d4a60628
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-23 23:44:59 +0600

    add some method

--- src/main/java/com/shahariyr/RestCrudApi/dao/EmployeeDAO.java
+++ src/main/java/com/shahariyr/RestCrudApi/dao/EmployeeDAO.java
@@ -7,4 +7,11 @@ import com.shahariyr.RestCrudApi.entity.Employee;
 public interface EmployeeDAO {
 
 	List<Employee> findall();
+	
+	Employee findById(int theId);
+	
+	Employee savEmployee(Employee thEmployee);
+	
+	void deleteEmployee(int theId);
+	
 }
