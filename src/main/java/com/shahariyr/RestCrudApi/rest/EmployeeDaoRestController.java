e27ac0b904e17e3d4dee784c9cca204bca1f976a
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-10-25 22:34:17 +0600

    update contoller

--- src/main/java/com/shahariyr/RestCrudApi/rest/EmployeeDaoRestController.java
+++ src/main/java/com/shahariyr/RestCrudApi/rest/EmployeeDaoRestController.java
@@ -2,29 +2,47 @@ package com.shahariyr.RestCrudApi.rest;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.shahariyr.RestCrudApi.dao.EmployeeDAO;
 import com.shahariyr.RestCrudApi.entity.Employee;
+import com.shahariyr.RestCrudApi.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeDaoRestController {
 
-	
-	///Constructor Injection
-	private EmployeeDAO employeeDAO;
+	/*
+	 * No need this injection. employeeService will handle this from now
+	 * ///Constructor Injection private EmployeeDAO employeeDAO;
+	 * 
+	 * public EmployeeDaoRestController(EmployeeDAO employeeDAO) { this.employeeDAO
+	 * = employeeDAO; }
+	 */
 
-	public EmployeeDaoRestController(EmployeeDAO employeeDAO) {
-		this.employeeDAO = employeeDAO;
+	private EmployeeService employeeService;
+
+	public EmployeeDaoRestController(EmployeeService thEmployeeService) {
+		this.employeeService = thEmployeeService;
 	}
 
-	
 	@GetMapping("/employees")
 	public List<Employee> findEmployees() {
-		
-		return employeeDAO.findall();
+
+		return employeeService.findall();
+	}
+
+	@GetMapping("/employees/{employeeID}") // "{employeeID}" and after @pathvariable "employeeID" must be same
+										
+	public Employee getSingleEmployee(@PathVariable int employeeID) {
+
+		Employee theEmployee = employeeService.findById(employeeID);
+
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee not found which ID is :" + employeeID);
+		}
+
+		return theEmployee;
 	}
 
 }
