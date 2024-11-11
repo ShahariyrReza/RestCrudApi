0b45161a1de0335ce6e1603996397b07484a637e
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-11 10:31:40 +0600

    update imp class

--- src/main/java/com/shahariyr/RestCrudApi/service/EmployeeServiceImp.java
+++ src/main/java/com/shahariyr/RestCrudApi/service/EmployeeServiceImp.java
@@ -1,11 +1,10 @@
 package com.shahariyr.RestCrudApi.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.shahariyr.RestCrudApi.dao.EmployeeDAO;
+import com.shahariyr.RestCrudApi.Dao.EmployeeRepo;
 import com.shahariyr.RestCrudApi.entity.Employee;
 
 @Service
@@ -14,37 +13,35 @@ public class EmployeeServiceImp implements EmployeeService{
 	
 	
 	//Constructor Injection of employeeDao
-	private EmployeeDAO employeeDAO; // ekhn e ami EmployeeDao k "employeeDao" hisebe niyesi.. so niche return korte hobe ei akare
+	private EmployeeRepo employeeRepo; // ekhn e ami EmployeeDao k "employeeDao" hisebe niyesi.. so niche return korte hobe ei akare
 	
-	public EmployeeServiceImp(EmployeeDAO thEmployeeDAO) {
-		this.employeeDAO = thEmployeeDAO;
+	public EmployeeServiceImp(EmployeeRepo theEmployeeRepo) {
+		this.employeeRepo = theEmployeeRepo;
 	}
 	
 	
 	@Override
 	public List<Employee> findall() {
-		return employeeDAO.findall();
+		return employeeRepo.findAll();
 	}
 
-	// No Transactional annotation because this method didn't modify the database.
+	// No Transaction annotation because this method didn't modify the database.
 	@Override
-	public Employee findById(int theId) {		
-		return employeeDAO.findById(theId);
+	public Optional<Employee> findById(int theId) {		
+		return employeeRepo.findById(theId);
 	}
 
 
-	@Transactional
 	@Override
 	public Employee savEmployee(Employee thEmployee) {
 		
-		return employeeDAO.savEmployee(thEmployee);
+		return employeeRepo.save(thEmployee);
 	}
 
 
-	@Transactional
 	@Override
 	public void deleteEmployee(int theId) {
-		employeeDAO.deleteEmployee(theId);
+		employeeRepo.deleteById(theId);
 		
 	}
 
