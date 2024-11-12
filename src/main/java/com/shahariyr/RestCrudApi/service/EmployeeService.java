9d9e1953a9b8d762c4bf1c4d67cefae9c614a9ab
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-12 09:15:01 +0600

    Optional method work for findById

--- src/main/java/com/shahariyr/RestCrudApi/service/EmployeeService.java
+++ src/main/java/com/shahariyr/RestCrudApi/service/EmployeeService.java
@@ -1,17 +1,16 @@
 package com.shahariyr.RestCrudApi.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.shahariyr.RestCrudApi.entity.Employee;
 
 public interface EmployeeService {
 
-	List<Employee> findall();
-	
-	Optional<Employee> findById(int theId);
-	
+	List<Employee> findAll();
+
+	Employee findById(int theId);
+
 	Employee savEmployee(Employee thEmployee);
-	
+
 	void deleteEmployee(int theId);
 }
