00b77c93beaaaa83d338357015aff411231d6125
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-14 00:00:59 +0600

    add rest resource path as "employee"

--- src/main/java/com/shahariyr/RestCrudApi/dao/EmployeeRepo.java
+++ src/main/java/com/shahariyr/RestCrudApi/dao/EmployeeRepo.java
@@ -1,9 +1,11 @@
 package com.shahariyr.RestCrudApi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.shahariyr.RestCrudApi.entity.Employee;
 
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 
 }
